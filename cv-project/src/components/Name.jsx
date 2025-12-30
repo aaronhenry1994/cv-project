bca10b436e95ee97460859e741dc8a8d1d867262
Aaron Henry <aaronhenry1994@outlook.com>
2025-12-29 19:32:28 -0700

    Adding original app to the react environment

--- cv-project/src/components/Name.jsx
+++ cv-project/src/components/Name.jsx
@@ -1,3 +1,24 @@
-export default function Name() {
-    
+function Name() {
+    const [name, email, phone] = useState('');
+    const [submitted, SetIsSubmitted] = useState(false);
+
+    return (
+        <div className="Personal">
+            <div className="Name">
+                <h2>Name:</h2>
+                <input type="text" value={name} />
+            </div>
+            <div className="Phone">
+                <h2>Phone Number:</h2>
+                <input type="text" value={phone} />
+            </div>
+            <div className="Email">
+                <h2>Name</h2>
+                <input type="text" value={email} />
+            </div>
+            <button onClick={() => SetIsSubmitted(true)}>Submit</button>
+        </div>
+    )
 }
+
+export default Name
