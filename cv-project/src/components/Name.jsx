dcc66dfe522515e54d5de7a9635f410da355642f
Aaron Henry <aaronhenry1994@outlook.com>
2026-01-15 18:37:19 -0700

    Can update textboxes, and reset them. Just need to get it so you can reset them.

--- cv-project/src/components/Name.jsx
+++ cv-project/src/components/Name.jsx
@@ -1,22 +1,57 @@
+import React, {useState} from 'react';
+
 function Name() {
-    const [name, email, phone] = useState('');
-    const [submitted, SetIsSubmitted] = useState(false);
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [submitted, setIsSubmitted] = useState(false);
+    const [reset, setResetValue] = useState('');
+
+    let nameBox = document.getElementById('nameBox')
+    let phoneBox = document.getElementById('phoneBox')
+    let emailBox = document.getElementById('emailBox')
+
+    let parentNode = document.getElementById('parent');
+
+
+    function handleName() {
+        setName(nameBox.value)
+    };
+
+    function handlePhone() {
+        setPhone(phoneBox.value)
+    }
+
+    function handleEmail() {
+        setEmail(emailBox.value)
+    }
+
+    function handleSubmit() {
+        
+    }
+
+    function handleReset() {
+        setName('')
+        setPhone('')
+        setEmail('')
+    }
 
     return (
-        <div className="Personal">
+        <div className="Personal" id='parent'>
             <div className="Name">
                 <h2>Name:</h2>
-                <input type="text" value={name} />
+                <input type="text" id='nameBox' value={name} onChange={handleName}/>
             </div>
             <div className="Phone">
                 <h2>Phone Number:</h2>
-                <input type="text" value={phone} />
+                <input type="text" id='phoneBox' value={phone} onChange={handlePhone}/>
             </div>
             <div className="Email">
-                <h2>Name</h2>
-                <input type="text" value={email} />
+                <h2>Email</h2>
+                <input type="text" id='emailBox' value={email}  onChange={handleEmail}/>
             </div>
-            <button onClick={() => SetIsSubmitted(true)}>Submit</button>
+            <button onClick={() => handleSubmit}>Submit</button>
+            <button onClick={handleReset}>Reset</button>
         </div>
     )
 }
